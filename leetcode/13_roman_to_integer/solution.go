3ce04f9f7025d6253aa4f0bb680ddc8d85cbb22c
Celestian <celestian.verse@gmail.com>
2026-07-10 16:19:54 +0300

    feat: #13 Roman to Integer

---
+++ leetcode/13_roman_to_integer/solution.go
@@ -0,0 +1,27 @@
+package solution
+
+func romanToInt(s string) int {
+	values := [256]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	result := 0
+
+	for i := range s {
+		j := i + 1
+		current := values[s[i]]
+
+		if j < len(s) && current < values[s[j]] {
+			result -= current
+		} else {
+			result += current
+		}
+	}
+
+	return result
+}
