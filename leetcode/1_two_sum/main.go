c095fc2178528e1ec033999b58f5963f8bb97243
Celestian <celestian.verse@gmail.com>
2026-07-11 14:45:30 +0300

    feat: #1 Two Sum

--- leetcode/1_two_sum/main.go
+++ leetcode/1_two_sum/main.go
@@ -1,4 +1,13 @@
-package solution
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 9, 7, 15, 11}
+	target := 18
+
+	fmt.Println(twoSum(nums, target))
+}
 
 func twoSum(nums []int, target int) []int {
 	seen := make(map[int]int, len(nums))
