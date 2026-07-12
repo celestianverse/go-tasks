45716fc1b6b698f50699db85d82754195826bea6
Celestian <celestian.verse@gmail.com>
2026-07-12 18:51:44 +0300

    feat: #27 Remove Element

---
+++ leetcode/27_remove_element/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	input := []int{2, 9, 8, 2, 7, 6, 5, 2}
+
+	k := removeElement(input, 2)
+
+	fmt.Println(k, input[:k])
+}
+
+func removeElement(nums []int, val int) int {
+	k := 0
+
+	for _, v := range nums {
+		if v != val {
+			nums[k] = v
+			k++
+		}
+	}
+
+	return k
+}
