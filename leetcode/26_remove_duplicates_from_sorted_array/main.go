079b0a791f838cecfab496edb60f16d32d68360c
Celestian <celestian.verse@gmail.com>
2026-07-12 15:02:44 +0300

    feat: #26 Remove Duplicates from Sorted Array

---
+++ leetcode/26_remove_duplicates_from_sorted_array/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	input := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+
+	k := removeDuplicates(input)
+
+	fmt.Println(k, input[:k])
+}
+
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	k := 1
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+
+	return k
+}
